4ae9917e2a762c56cc320adc3492f9538ba36197
chasura416 <chasura416@naver.com>
2023-09-12 10:38:46 +0900

    메뉴 만들기

---
+++ src/components/menu.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Menu = styled.div`
+  width: 400px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MenuBtn = styled.button`
+
+`;
