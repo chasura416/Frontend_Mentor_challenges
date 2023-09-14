2135c5563c97915d5c7e6a1d80e1e7840d321843
chasura416 <chasura416@naver.com>
2023-09-14 10:19:26 +0900

    main2 추가작업

---
+++ src/layout2/mainLayout.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const MainLayout = styled.div`
+  width: 1440px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+`;
