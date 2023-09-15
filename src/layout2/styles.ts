41ebd57b51897ef0ae5723c5a63b8d72916ca603
chasura416 <chasura416@naver.com>
2023-09-15 10:57:31 +0900

    Main2 Layout 설정

---
+++ src/layout2/styles.ts
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+
+export const Layout = styled.div`
+  /* width: 100%; */
+  min-width: 1440px;
+`;
+
+export const ContainerDiv = styled.div`
+  display: flex;
+`;
+
+export const LeftContainer = styled.div`
+  width: 720px;
+  height: 837px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightContainer = styled.div`
+  width: 720px;
+  height: 837px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+`;
+
+export const UpperBoxWrapper = styled.div`
+  display: flex;
+`;
+
+export const LowerBoxWrapper = styled.div`
+  display: flex;
+`;
--- src/pages/main2.tsx
+++ src/pages/main2.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import * as St from '../layout2/styles'
 
 const main2 = () => {
   return (
-    <div>
-      as
-    </div>
+    <St.Layout>
+      <St.ContainerDiv>
+        <St.LeftContainer>상단 왼쪽</St.LeftContainer>
+        <St.RightContainer>상단 오른쪽</St.RightContainer>
+      </St.ContainerDiv>
+      <St.BoxWrapper>
+        <St.UpperBoxWrapper>
+          <div>상단 박스1</div>
+          <div>상단 박스2</div>
+          <div>상단 박스3</div>
+        </St.UpperBoxWrapper>
+        <St.LowerBoxWrapper>
+          <div>하단 박스1</div>
+          <div>하단 박스2</div>
+          <div>하단 박스3</div>
+        </St.LowerBoxWrapper>
+      </St.BoxWrapper>
+    </St.Layout>
   );
 };
 
